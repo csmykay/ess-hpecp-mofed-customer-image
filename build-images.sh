3820033cc5e4472e4878f23e137ac2d6aa85b439
Chad Smykay <csmykay@ion.local>
2020-04-17 15:45:54 -0500

    initial build

---
+++ build-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#https://docs.docker.com/engine/reference/commandline/build/
+DATE=$(date +%Y-%m-%d_%H:%M:%S)
+VERSION=1.0
+if [ -z $CURRENT ]; then
+	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+fi
+docker build --build-arg BUILD_DATE=$DATE --build-arg BUILD_VERSION=$VERSION --no-cache=true --rm -f Dockerfile -t ess-hpecp-mofed-customer-image:$VERSION .
+docker tag ess-hpecp-mofed-customer-image:$VERSION ess-hpecp-mofed-customer-image:latest
+docker tag ess-hpecp-mofed-customer-image csmykay/ess-hpecp-mofed-customer-image:latest
+docker login
+docker push ccsmykay/ess-hpecp-mofed-customer-image:latest 
